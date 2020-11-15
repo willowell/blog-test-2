7628858154e28525622cfc436388bf8236737510
William Howell <wlm.howell@gmail.com>
2020-11-14 23:38:25 -0500

    Init

---
+++ gatsby-config.js
@@ -0,0 +1,144 @@
+///==============================================================================================///
+///                                    SITE METADATA                                             ///
+///==============================================================================================///
+module.exports = {
+  siteMetadata: {
+    title: 'William Howell',
+    author: {
+      name: 'William Howell',
+    },
+    description: 'My little plot of digital land',
+    siteUrl: 'https://www.willhow.com',
+  },
+
+///==============================================================================================///
+///                                            META                                              ///
+///==============================================================================================///
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    
+    'gatsby-plugin-netlify',
+
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'William Howell',
+        short_name: 'William Howell',
+        description: 'My little plot of digital land',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#673ab7',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/img/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/img/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    },
+
+    'gatsby-plugin-sitemap',
+
+    'gatsby-plugin-offline',
+
+
+///==============================================================================================///
+///                              IMAGES AND STATIC DATA                                          ///
+///==============================================================================================///
+    'gatsby-plugin-sharp',
+
+    'gatsby-transformer-sharp',
+
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/content/posts/`,
+        name: 'posts',
+      },
+    },
+
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/content/images/`,
+        name: 'images',
+      },
+    },
+
+///==============================================================================================///
+///                                          MARKDOWN                                            ///
+///==============================================================================================///
+
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+        
+          /// IMAGES
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1140,
+              quality: 90,
+              linkImagesToOriginal: false,
+              wrapperStyle: 'margin-bottom: 1rem;',
+            },
+          },
+          
+          /// IFRAMES
+          {
+            resolve: 'gatsby-remark-responsive-iframe',
+            options: {
+              wrapperStyle: 'margin-bottom: 1.0725rem',
+            },
+          },
+
+          /// CODE HIGHLIGHTS
+          {
+            resolve: 'gatsby-remark-prismjs',
+            options: {
+              classPrefix: 'language-',
+              inlineCodeMarker: '>',
+              aliases: {},
+              showLineNumbers: true,
+              noInlineHighlight: false,
+              prompt: {
+                user: 'root',
+                host: 'localhost',
+                global: true
+              }
+            }
+          },
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+        ],
+      },
+    },
+
+    'gatsby-plugin-catch-links',
+    
+///==============================================================================================///
+///                                         TYPESCRIPT                                           ///
+///==============================================================================================///
+    'gatsby-plugin-typescript',
+
+    {
+      resolve: 'gatsby-plugin-graphql-codegen',
+      options: {
+        fileName: `types/graphql-types.d.ts`,
+      },
+    },
+
+///==============================================================================================///
+///                                            SASS                                              ///
+///==============================================================================================///
+    'gatsby-plugin-sass',
+  ],
+}
---
+++ src/pages/404.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import { siteMetadata } from '../../gatsby-config'
+
+import Meta from '../components/meta/meta'
+import Layout from '../components/layout/layout'
+
+interface Props {
+    location: Location
+}
+
+const NotFound: React.FC<Props> = ({ location }: Props) => {
+  return (
+    <Layout location={ location } >
+      <Meta site={ siteMetadata } title='404' />
+      <div>
+        <section className='text-center'>
+          <div className='container'>
+            <header>
+              <h1>404</h1>
+              <p className='h2'>Whoops!</p>
+              <img src='https://http.cat/404' />
+            </header>
+          </div>
+        </section>
+      </div>
+    </Layout>
+  )
+}
+
+export default NotFound
